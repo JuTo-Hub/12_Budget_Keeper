432e3ac799f509c198a0b11f08c2203604b0ef83
JuTo_Hub <JustinTorres1@Outlook.com>
2020-11-23 11:26:36 -0600

    Added Mock Data for testing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
-  
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
+import './App.css';
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { AgGridColumn, AgGridReact } from 'ag-grid-react';
@@ -54,7 +27,6 @@ const App = () => {
 
   return (
     <div className="ag-theme-alpine" style={{ height: 1000, width: 1100 }}>
-      {/* {this.state.employees.map(employee => ( */}
       <AgGridReact
         onGridReady={onGridReady}
         rowData={rowData}>
